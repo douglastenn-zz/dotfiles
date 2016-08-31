ffa6156576139c689518306304e1dd43db58022a
douglastenn <douglas.develop@gmail.com>
2016-08-31 17:09:14 -0300

    First Commit

---
+++ atom/packages.sh
@@ -0,0 +1,21 @@
+# Packages
+apm install stylus
+apm install atom-beautify
+apm install autocomplete-modules
+apm install csscomb
+apm install docblockr
+apm install emmet
+apm install emmet-jsx-css-modules
+apm install file-icons
+apm install language-babel
+apm install markdown-writer
+apm install merge-conflicts
+apm install pigments
+apm install project-manager
+apm install auto-update-packages
+
+# Themes
+apm install dracula-theme
+apm install dracula-ui
+apm install material-syntax
+apm install material-ui
